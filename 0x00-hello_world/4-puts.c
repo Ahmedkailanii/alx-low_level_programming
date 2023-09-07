1ceb89b07c4eb983ed12a86ea59f03f08d81cfc4
ahmed kailany <ahmedkailany98@gmail.com>
2023-09-07 20:13:03 +0200

    losser

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,3 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: A C program that print with put function
- *
- * Return: Alawys 0 (Success)
-*/
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+
+int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }

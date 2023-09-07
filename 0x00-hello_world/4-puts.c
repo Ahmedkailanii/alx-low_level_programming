416e0fd0fe8a52272624ab8fc6f3b03bdfa26c75
ahmed kailany <ahmedkailany98@gmail.com>
2023-09-07 19:58:40 +0200

    losser

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,5 @@
  * Return: Alawys 0 (Success)
 */ 
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
- return (0);
+{puts("\"Programming is like building a multilingual puzzle"); return (0);
 } 

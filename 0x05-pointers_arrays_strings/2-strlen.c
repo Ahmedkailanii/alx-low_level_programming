315844eb1102d49a420f35449a167cffef66c2c1
ahmed kailany <ahmedkailany98@gmail.com>
2023-09-18 07:49:49 +0200

    please

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,23 +9,13 @@
  * Return: length
 
  */
-
 int _strlen(char *s)
-
 {
-
-        int longi = 0;
-
-        while (*s != '\0')
-
-        {
-
-                longi++;
-
-                s++;
-
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
         }
-
-        return (longi);
-
+return (longi);
 }
